c9efebbadd2b7a72a9a6fd209d72739182059ad4
cow0731 <cow0731@gmail.com>
2024-01-16 17:19:43 +0900

    first commit

---
+++ src/main/java/com/group/happycard/HappyCardApplication.java
@@ -0,0 +1,13 @@
+package com.group.happycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/happycard/HappyCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.happycard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
