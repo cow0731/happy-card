cd993ff90a91172037b46d365e0dadc238fd2471
cow0731 <cow0731@gmail.com>
2024-01-26 15:07:52 +0900

    feat: 카드 내용 작성 페이지(write-card) textarea 높이 자동 조절 구현

---
+++ src/main/resources/static/js/card.js
@@ -0,0 +1,18 @@
+// Textarea 자동 높이 조절
+function autoResize(textarea) {
+    if (textarea.scrollHeight > textarea.clientHeight) {
+        textarea.style.height = textarea.scrollHeight + "px";
+    }
+    else {
+        if(textarea.scrollHeight > 106) {
+            textarea.style.height = (textarea.scrollHeight-24) + "px"
+            autoResize(textarea);
+        }
+    }
+}
+
+// 페이지 로드 시 초기 호출
+window.addEventListener('load', function() {
+    autoResize(document.getElementById('textarea'));
+});
+
