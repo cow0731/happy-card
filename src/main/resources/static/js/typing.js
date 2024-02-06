f20ccd84e3cfd70b74f7750f349662f8348cd353
bone0825 <quddlf775@naver.com>
2024-02-06 14:39:07 +0900

    update textarea resize

--- src/main/resources/static/js/typing.js
+++ src/main/resources/static/js/typing.js
@@ -1,5 +1,5 @@
 const target = document.querySelector("#typingTextarea"); //html에서 dynamic 선택
-
+const area = document.getElementById("typingTextarea");
 //함수 만들기
 function blink(){
     target.classList.toggle("active"); // dynamic에 active 클래스 추가<->삭제 반복
@@ -14,12 +14,16 @@ const stringLen = string.split('\n').length;
 console.log(string)
 console.log(stringLen)
 
-document.getElementById("typingTextarea").style.height = 106 + (24 * stringLen) +'px';
 
 const split = string.split(""); //string의 텍스트를 여러개의 문자열로 나눠줌
 
+function resizeArea(){
+    area.style.height = area.scrollHeight +'px';
+}
+
 //문자열을 한개씩 나타내주는 함수 만들기
 function dynamic(arr){
+    resizeArea();
     if(arr.length > 0){ //배열의 길이가 0보다 크면(배열에 요소가 하나라도 있다면)
         target.textContent += arr.shift();//dynamic에 textContent에 배열의 요소 추가
         setTimeout(function(){
